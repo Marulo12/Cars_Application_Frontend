b8c4dadbe4c2c0122bc2d11929ac3ff66b59af86
mboscatto <mariolucbos@gmail.com>
2023-07-12 14:55:54 -0300

    routing to cars module

--- src/app/pages/cars/cars-routing.module.ts
+++ src/app/pages/cars/cars-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarsComponent } from './cars.component';
+import { ListCarsComponent } from './components/list-cars/list-cars.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CarsComponent,
+    children: [
+      { path: '', redirectTo: 'list-cars', pathMatch: 'full' },
+      { path: 'list-cars', component: ListCarsComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CarsRoutingModule { }
+export class CarsRoutingModule {}
--- src/app/pages/cars/cars.module.ts
+++ src/app/pages/cars/cars.module.ts
@@ -3,11 +3,13 @@ import { CarsRoutingModule } from './cars-routing.module';
 import { CommonsModule } from 'src/app/shared/modules/commons/commons.module';
 import { PrimengModule } from 'src/app/shared/modules/primeng/primeng.module';
 import { CarsComponent } from './cars.component';
+import { ListCarsComponent } from './components/list-cars/list-cars.component';
 
 
 @NgModule({
   declarations: [
-    CarsComponent
+    CarsComponent,
+    ListCarsComponent
   ],
   imports: [
     CommonsModule,
